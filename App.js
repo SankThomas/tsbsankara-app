e1f2d34cc02e2816d068c4e11e0ac439ef41fed5
sankthomas <thomassankara74@gmail.com>
2023-09-09 23:13:49 +0300

    react navigation routing

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Screens
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Blog from "./screens/Blog";
+import Books from "./screens/Books";
+import Contact from "./screens/Contact";
+import Portfolio from "./screens/Portfolio";
+import Projects from "./screens/Projects";
+import SingleBlog from "./screens/SingleBlog";
+import SingleBooks from "./screens/SingleBooks";
+import SinglePortfolio from "./screens/SinglePortfolio";
+import SingleProject from "./screens/SingleProject";
+import Youtube from "./screens/Youtube";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="dark" />
+
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          animation: "slide_from_right",
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Blog" component={Blog} />
+        <Stack.Screen name="SingleBlog" component={SingleBlog} />
+        <Stack.Screen name="Books" component={Books} />
+        <Stack.Screen name="SingleBooks" component={SingleBooks} />
+        <Stack.Screen name="Contact" component={Contact} />
+        <Stack.Screen name="Portfolio" component={Portfolio} />
+        <Stack.Screen name="SinglePortfolio" component={SinglePortfolio} />
+        <Stack.Screen name="Projects" component={Projects} />
+        <Stack.Screen name="SingleProject" component={SingleProject} />
+        <Stack.Screen name="Youtube" component={Youtube} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
