29b5474dae8e4158f869a3dd2117be8c351943c7
sankthomas <thomassankara74@gmail.com>
2023-12-07 00:34:58 +0300

    project data

---
+++ data/data.js
@@ -0,0 +1,152 @@
+export const images = [
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/clipboard.png?raw=true",
+    title: "Clipboard landing page",
+    description:
+      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/blogr.png?raw=true",
+    title: "Blogr Website",
+    description:
+      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/kodidi-consulting.png?raw=true",
+    title: "Kodidi Consulting",
+    description:
+      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/mental-health-zambia.png?raw=true",
+    title: "Mental Health Care Zambia",
+    description:
+      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/twembo.png?raw=true",
+    title: "Twembochi Mununga Portfolio",
+    description:
+      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/wahu.png?raw=true",
+    title: "Margaret Wahu Portfolio",
+    description:
+      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+  },
+];
+
+export const books = [
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/htmlcourse.png?raw=true",
+    title: "HTML and CSS Course",
+    description:
+      "Learn modern web development using HTML5 semantics by building a Model Hotel Website and deploying it to the internet.",
+    runtime: "5hr 58mins",
+    price: "$19.99",
+    objectives: [
+      "Build a modern website",
+      "Deploy websites to the internet",
+      "Learn modern HTML5 Semantics",
+      "Learn moden web development practices",
+    ],
+    instructor: [
+      {
+        name: "Thomas Sankara",
+        imagepath: "author.png",
+        about:
+          "Front-end web developer with over 4 years experience in the modern web development scene, practicing ReactJs and TailwindCSS.",
+      },
+    ],
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/reactjs.jpeg?raw=true",
+    title: "20 ReactJs Projects",
+    description:
+      "Build 20 ReactJs Projects with increasing complexity and functionality. This is a beginner friendly course that you can learn at any level.",
+    runtime: "12hours",
+    price: "$94.99",
+    objectives: [
+      "Establish ReactJs fundamentals",
+      "Deploy React web apps to the internet",
+      "Learn best practices for React",
+    ],
+    instructor: [
+      {
+        name: "Thomas Sankara",
+        imagepath: "author.png",
+        about:
+          "Front-end web developer with over 4 years experience in the modern web development scene, practicing ReactJs and TailwindCSS.",
+      },
+    ],
+  },
+];
+
+export const projects = [
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/dev-to-clone.png?raw=true",
+    title: "Dev.to Clone",
+    description:
+      "I built a partial clone of the popular Dev.to Blogging Website. It is the center section of the website featuring pagination, filter by tags, and fetching API data.",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/diarry.png?raw=true",
+    title: "Diarry",
+    description:
+      "Get your fingers ready to make entries into this new application. Built in ReactJs, it is fast by default and saves all the entries you make into localStorage so you can retrieve it anytime.",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/invoicerv2.png?raw=true",
+    title: "Invoicing Application",
+    description:
+      "I built an invoicing application with ReactJs. You can enter client details, add and delete invoice items and of course edit your entries. It is already released on ProductHunt as well.",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/markdown-editor.png?raw=true",
+    title: "Markdown Editor",
+    description:
+      "I built a markdown editor/converter in ReactJs. It takes text that you type in, converts it to markdown then displays it to you.",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/search-filter.png?raw=true",
+    title: "Input Search Filter",
+    description:
+      "A simple filtering app using the Black History API that I made, built in ReactJs and styled using Tailwind CSS.",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/blog.png?raw=true",
+    title: "Personal Blog using Sanity CMS and ReactJs",
+    description:
+      "I built a personal blog using Sanity Headless Content Management System and ReactJs. Blog data is fetched from the Sanity API backend, styling is done using Tailwind CSS.",
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/projects/password-generator.png?raw=true",
+    title: "Password Generator",
+    description:
+      "Use this app to generate random password for your social media accounts - or for fun - with the added functionality to copy your password to the clipboard using the Clipboard API.",
+  },
+];
+
+export const videos = [
+  {
+    title: "Frontend Mentor FAQs Accordion #tsbsankara",
+    id: "O4Tl85MzvNs",
+  },
+  {
+    title: "Let's build an Invoice App",
+    id: "rJvZDWN_sNY",
+  },
+  {
+    title: "Rest Countries API challenge from Frontend Mentor using ReactJS",
+    id: "vwZtmHSa9OY",
+  },
+  {
+    title: "QR Code Component - Frontend Mentor Challenge",
+    id: "JFyMWwOxHYM",
+  },
+  {
+    title: "Order Summary Component - Frontend Mentor Challenge",
+    id: "uaM5F8O_VI8",
+  },
+];
