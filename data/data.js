4dbd0baec085efcb04bcc0150a711179ea6dec6a
sankthomas <thomassankara74@gmail.com>
2023-12-08 13:47:37 +0300

    update app data

--- data/data.js
+++ data/data.js
@@ -3,43 +3,43 @@ export const images = [
     path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/clipboard.png?raw=true",
     title: "Clipboard landing page",
     description:
-      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+      "Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.",
   },
   {
     path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/blogr.png?raw=true",
     title: "Blogr Website",
     description:
-      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+      "Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.",
   },
   {
     path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/kodidi-consulting.png?raw=true",
     title: "Kodidi Consulting",
     description:
-      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+      "Kodidi Consulting is a consulting firm that features teaching, online classes, report writing, and, obviously, consultation services.",
   },
   {
     path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/mental-health-zambia.png?raw=true",
     title: "Mental Health Care Zambia",
     description:
-      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+      "Mental Health Care Zambia is a Non-Profit Organization that was formed as a solution to the increasing number of mental illnesses that were left untreated for different reasons. One of the significant reasons is the stigma that has been tied to mental health in Africa. The organization seeks to completely erase the belief that mental illness is “Madness” the organization was formed to make it easier for people to have access to mental health institutions and access professional help to reduce the mental illnesses and reduce the suicide rate among the people of Zambia. assdsddad asd asd sadas das dasd asd asdsada d asdas dsa das das das das dasd asd asd asdas da das d",
   },
   {
     path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/twembo.png?raw=true",
     title: "Twembochi Mununga Portfolio",
     description:
-      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+      "A custom designed and built personal website for Twembochi Mununga. Built in ReactJs, it is fast, lightweight and boasts animations that use the Framer Motion Library, and it is styled using Tailwind CSS.",
   },
   {
     path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/portfolio/wahu.png?raw=true",
     title: "Margaret Wahu Portfolio",
     description:
-      "Clipboard allows you to access anything you copy from any website. Simply install the browser extension and get access to everything you have ever copied. This might be a good thing or a bad thing depending on what you copy. :wink",
+      "Margaret Wahu is a Cybersecurity expert currently based in Nairobi. She holds a degree in Computer Science from The East African University in Kajiado.",
   },
 ];
 
-export const books = [
+export const courses = [
   {
-    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/htmlcourse.png?raw=true",
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/courses/htmlcss.png?raw=true",
     title: "HTML and CSS Course",
     description:
       "Learn modern web development using HTML5 semantics by building a Model Hotel Website and deploying it to the internet.",
@@ -61,7 +61,7 @@ export const books = [
     ],
   },
   {
-    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/reactjs.jpeg?raw=true",
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/courses/react.jpg?raw=true",
     title: "20 ReactJs Projects",
     description:
       "Build 20 ReactJs Projects with increasing complexity and functionality. This is a beginner friendly course that you can learn at any level.",
@@ -72,6 +72,56 @@ export const books = [
       "Deploy React web apps to the internet",
       "Learn best practices for React",
     ],
+    instructor: [
+      {
+        name: "Thomas Sankara",
+        imagepath: "author.png",
+        about:
+          "Front-end web developer with over 4 years experience in the modern web development scene, practicing ReactJs and TailwindCSS.",
+      },
+    ],
+  },
+];
+
+export const books = [
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/book-cover.png?raw=true",
+    title: "How I Became a web developer",
+    description: "Read about how I became a web developer",
+    length: "57 pages",
+    price: "$1",
+    instructor: [
+      {
+        name: "Thomas Sankara",
+        imagepath: "author.png",
+        about:
+          "Front-end web developer with over 4 years experience in the modern web development scene, practicing ReactJs and TailwindCSS.",
+      },
+    ],
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/fundamentals-book-cover.png?raw=true",
+    title: "Fundamentals of ReactJs: With Examples",
+    description:
+      "Bringing you from the fundamentals to a hero in ReactJs by explaining key points you need to understand as you become comfortable using the popular JavaScript framework.",
+    length: "85 pages",
+    price: "$1",
+    instructor: [
+      {
+        name: "Thomas Sankara",
+        imagepath: "author.png",
+        about:
+          "Front-end web developer with over 4 years experience in the modern web development scene, practicing ReactJs and TailwindCSS.",
+      },
+    ],
+  },
+  {
+    path: "https://github.com/SankThomas/tsbsankara-mobile-images/blob/main/books/book-advice.jpg?raw=true",
+    title: "Untitled - Pieces of Advice from a Young Man",
+    description:
+      "This is a 15-page mini book with pieces of advice about things I think about. I offer different views on some of the things I talk about, and for others, it boils down to common sense.",
+    length: "15 pages",
+    price: "$1",
     instructor: [
       {
         name: "Thomas Sankara",
